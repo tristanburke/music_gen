7644a215c037da3c806cec52a7799874126e8ff8
Tristan Burke <tristan_burke@berkeley.edu>
2017-05-24 20:21:59 -0400

    python

---
+++ Generate.py
@@ -0,0 +1,21 @@
+/**
+ * Main - Intializes "Muisc" Object -> runs improv until prompted to exit
+ * 
+ * @author 	Tristan Burke
+ * @version	1.0 - Feb 27, 2016
+ * 
+**/
+
+import java.util.PriorityQueue;
+import java.util.Comparator; 
+import java.util.ArrayList;
+
+public class Generate {
+
+	public Generate() {
+
+	}
+	public static void main(String[] args) {
+
+	}
+}
